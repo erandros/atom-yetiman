f1949ec8872e604e21d08580b987ed5059a86555
erandros <guidotarsia@gmail.com>
2017-03-06 12:04:57 -0300

    Initial commit

---
+++ spec/atom-yetiman-spec.js
@@ -0,0 +1,73 @@
+'use babel';
+
+import AtomYetiman from '../lib/atom-yetiman';
+
+// Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
+//
+// To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
+// or `fdescribe`). Remove the `f` to unfocus the block.
+
+describe('AtomYetiman', () => {
+  let workspaceElement, activationPromise;
+
+  beforeEach(() => {
+    workspaceElement = atom.views.getView(atom.workspace);
+    activationPromise = atom.packages.activatePackage('atom-yetiman');
+  });
+
+  describe('when the atom-yetiman:toggle event is triggered', () => {
+    it('hides and shows the modal panel', () => {
+      // Before the activation event the view is not on the DOM, and no panel
+      // has been created
+      expect(workspaceElement.querySelector('.atom-yetiman')).not.toExist();
+
+      // This is an activation event, triggering it will cause the package to be
+      // activated.
+      atom.commands.dispatch(workspaceElement, 'atom-yetiman:toggle');
+
+      waitsForPromise(() => {
+        return activationPromise;
+      });
+
+      runs(() => {
+        expect(workspaceElement.querySelector('.atom-yetiman')).toExist();
+
+        let atomYetimanElement = workspaceElement.querySelector('.atom-yetiman');
+        expect(atomYetimanElement).toExist();
+
+        let atomYetimanPanel = atom.workspace.panelForItem(atomYetimanElement);
+        expect(atomYetimanPanel.isVisible()).toBe(true);
+        atom.commands.dispatch(workspaceElement, 'atom-yetiman:toggle');
+        expect(atomYetimanPanel.isVisible()).toBe(false);
+      });
+    });
+
+    it('hides and shows the view', () => {
+      // This test shows you an integration test testing at the view level.
+
+      // Attaching the workspaceElement to the DOM is required to allow the
+      // `toBeVisible()` matchers to work. Anything testing visibility or focus
+      // requires that the workspaceElement is on the DOM. Tests that attach the
+      // workspaceElement to the DOM are generally slower than those off DOM.
+      jasmine.attachToDOM(workspaceElement);
+
+      expect(workspaceElement.querySelector('.atom-yetiman')).not.toExist();
+
+      // This is an activation event, triggering it causes the package to be
+      // activated.
+      atom.commands.dispatch(workspaceElement, 'atom-yetiman:toggle');
+
+      waitsForPromise(() => {
+        return activationPromise;
+      });
+
+      runs(() => {
+        // Now we can test for view visibility
+        let atomYetimanElement = workspaceElement.querySelector('.atom-yetiman');
+        expect(atomYetimanElement).toBeVisible();
+        atom.commands.dispatch(workspaceElement, 'atom-yetiman:toggle');
+        expect(atomYetimanElement).not.toBeVisible();
+      });
+    });
+  });
+});
---
+++ spec/atom-yetiman-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomYetimanView from '../lib/atom-yetiman-view';
+
+describe('AtomYetimanView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
